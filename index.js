6fdd300ce476aa0bf70de4221460b5d6c3e58829
Jonathan Prince <jonathan.prince@gmail.com>
2015-11-16 13:05:42 +0100

    create queue, add templater and tests

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 var merge = require('merge')
+var kue = require('kue')
+var templater = require('./lib/templater')
 
 module.exports = function (app, options) {
   app.mailer = {}
 
   options = merge({
     redisHost: 'localhost',
+    redisPort: '6379',
     templatePath: '/server/mailer/templates/'
   }, options)
 
-  app.mailer.addToQueue = function (template, data) {
-    // get template, add data, send to queue
+  var emails = kue.createQueue({
+    redis: {
+      host: options.redisHost,
+      port: options.redisPort
+    }
+  })
+
+  app.mailer.addToQueue = function (templateName, data) {
+    // get template
+    var template = templater.load(options.templatePath, templateName)
+    // add data
+    var emailContent = templater.compile(template, data)
+    // send to queue
+    emails.create('email', emailContent).save()
   }
+
+  emails.process('email', function (email, done) {
+    console.log('processed', email.id)
+    done()
+  })
 }
--- test/test.js
+++ test/test.js
@@ -2,7 +2,37 @@
 var loopback = require('loopback')
 var expect = require('chai').expect
 var component = require('../')
+var templater = require('../lib/templater')
 var app
+
+describe('templater', function () {
+  var templatePath = 'test/templates/'
+  var templateName = 'test-email'
+
+  describe('loading a template', function () {
+    it('should return a template string', function (done) {
+      var templateString = templater.load(templatePath, templateName)
+      expect(templateString).to.be.a('string')
+      expect(templateString).to.equal('Email for: {{to}}, with Content: {{text}}.\n')
+      done()
+    })
+  })
+
+  describe('compiling a template', function () {
+    it('should return compiled text', function (done) {
+      var data = {
+        to: 'test recipient',
+        text: 'test email text'
+      }
+      var expected = 'Email for: ' + data.to + ', with Content: ' + data.text + '.\n'
+      var template = templater.load(templatePath, templateName)
+      var emailContent = templater.compile(template, data)
+      expect(emailContent).to.be.a('string')
+      expect(emailContent).to.equal(expected)
+      done()
+    })
+  })
+})
 
 describe('mailer', function () {
   beforeEach(function () {
@@ -10,10 +40,18 @@ describe('mailer', function () {
     app.set('legacyExplorer', false)
     app.use(loopback.rest())
   })
+  var templateName = 'test-email'
+  var data = {
+    to: 'test recipient',
+    text: 'test email text'
+  }
 
   describe('when using defaults', function () {
     it('should do something', function (done) {
-      component(app, {})
+      component(app, {
+        templatePath: 'test/templates/'
+      })
+      app.mailer.addToQueue(templateName, data)
       expect('feature').to.be.ok
       done()
     })
