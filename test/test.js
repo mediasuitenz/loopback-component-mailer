9d18bc3721e0236b034fcfa32beab7b070d2d1d6
Mitch Statz <mitch@mediasuite.co.nz>
2018-09-12 09:04:56 +1200

    Updated version number and fixed tests.

--- test/test.js
+++ test/test.js
@@ -60,7 +60,7 @@ describe('mailer', function () {
         subject: 'Greet the World',
         text: 'Hello World',
         html: '<h1>Hello World</h1>'
-      }, function (info) {
+      }, function (err, info) {
         expect(info.message).to.equal('success')
         done()
       })
@@ -95,7 +95,7 @@ describe('Integration test', function () {
         },
         templatePath: 'test/templates/'
       })
-      app.mailer.send(templateName, data, function (result) {
+      app.mailer.send(templateName, data, function (err, result) {
         expect(result).to.be.an('object')
         expect(result.status).to.equal('added to queue')
         done()
